981185a1369e453766cd91f099bd9d9756f1401c
Tran Thien Thanh <thanh@ignitemedicaltech.com>
2026-03-31 21:52:29 +0700

    release: bump plugin version to 5.5.5.6

--- plug.php
+++ plug.php
@@ -1,9 +1,19 @@
-Test a1
+<?php
+/*
+Plugin Name: AutoRunScriptAction
+Description: Auto run script action for WordPress.
+Version: 5.5.5.6
+Author: Tran Thien Thanh
+*/
 
-Dev2
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-duck
+define('ARSA_PLUGIN_VERSION', '5.5.5.6');
+define('ARSA_PLUGIN_FILE', __FILE__);
+define('ARSA_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('ARSA_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-sdsd
 
-sdsd
+define('ARSA_PLUGIN_FILE2', __FILE__);
